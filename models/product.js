7e2475aa49f42fc8f62d418979928fcb3408ef13
partha99saha <psaha.bdn9@gmail.com>
2021-12-28 08:41:02 +0530

    updated to NoSQL

--- models/product.js
+++ models/product.js
@@ -2,18 +2,26 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
-
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    //update the product
+    let dbOp;
+    if(this._id){
+    dbOp = db.collection('products')
+    .updateOne({_id : this._id},{$set: this});
+    } 
+    else{
+    dbOp = db.collection('products')
+    .insertOne(this)
+    }
+    return dbOp
       .then(result => {
         //console.log(result);
       })
@@ -36,6 +44,7 @@ class Product {
         console.log(err);
       });
   }
+
   static findById(prodID) {
     const db = getDb();
     return db
@@ -44,6 +53,21 @@ class Product {
       .next()
       .then(product => {
         //console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodID) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({_id: new mongodb.ObjectId(prodID)})
+      .then(product => {
+        //console.log(product);
+        console.log("product deleted");
         return product;
       })
       .catch(err => {
