8f380ffb71b5088ccd1cfa1748a47c8f20bc365c
partha99saha <psaha.bdn9@gmail.com>
2022-01-09 13:01:51 +0530

    validation added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const mailer = require("./mail");
+const { validationResult } = require('express-validator/check');
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -64,44 +65,40 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.postSignup = (req, res, next) => {
-  const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash(
-          "error",
-          "e-Mail exists already, please use a different email."
-        );
-        return res.redirect("/signup");
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            name: name,
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect("/login");
-          return mailer({
-            from: process.env.EMAIL,
-            to: req.body.email,
-            subject: "Thanks For Signup",
-            html: "<h1>You successfully signed up!</h1>",
-            attachments: [],
-          }).catch((err) => {
-            console.log(err);
-          });
-        });
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] }
+      });
+      return user.save();
     })
-    .catch((err) => {
+    .then(result => {
+      res.redirect('/login');
+      return mailer({
+        from: process.env.EMAIL,
+        to: req.body.email,
+        subject: "Thanks For Signup",
+        html: "<h1>You successfully signed up!</h1>",
+        attachments: [],
+      })
+    })
+    .catch(err => {
       console.log(err);
     });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,25 +1,62 @@
 const express = require('express');
-
+const { check, body } = require('express-validator/check');
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
-
 router.get('/signup', authController.getSignup);
-
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email.')
+            .custom((value, { req }) => {
+                return User.findOne({ email: value }).then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject(
+                            'E-Mail exists already, please pick a different one.'
+                        );
+                    }
+                });
+            }),
+        body(
+            'password',
+            'Please enter a password with only numbers and text and at least 5 characters.'
+        )
+            .isLength({ min: 5 })
+            .isAlphanumeric(),
+        body('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords have to match!');
+            }
+            return true;
+        })
+    ],
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
 router.get('/reset', authController.getReset);
-
 router.post('/reset', authController.postReset);
-
 router.get('/reset/:token', authController.getNewPassword);
 
-router.post('/new-password', authController.postNewPassword);
+router.post('/new-password', body(
+    'password',
+    'Please enter a password with only numbers and text and at least 5 characters.'
+)
+    .isLength({ min: 5 })
+    .isAlphanumeric(),
+    body('confirmPassword').custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Passwords have to match!');
+        }
+        return true;
+    })
+    , authController.postNewPassword);
 
 module.exports = router;
